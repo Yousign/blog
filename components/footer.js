cabaae33ddf38a475a627b04df6f386b9afcebd9
jérôme boileux <jerome.boileux@yousign.com>
2021-02-04 00:36:52 +0100

    ✏️ clean footer

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <Container>
         <div className="py-12 flex flex-col lg:flex-row items-center">
           <h3 className="typography-signature tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            The simpler way of saying yes!
+            The simpler way of saying yes
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a href="https://yousign.com/" className="mx-3 font-bold hover:underline">
