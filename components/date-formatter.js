91844976bac2af53b878f8136e61f35bbe60ef82
jérôme boileux <jerome.boileux@yousign.com>
2021-02-03 21:40:46 +0100

    🔧 correct datatypes serialize in getStaticProps

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -1,6 +1,5 @@
-import { parseISO, format } from 'date-fns';
+import { format } from 'date-fns';
 
 export default function DateFormatter({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>;
+  return <time dateTime={dateString}>{format(dateString, 'LLLL d, yyyy')}</time>;
 }
