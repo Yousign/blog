563db88886fcd9d54ebc291c3e287f0acde6b677
jérôme boileux <jerome.boileux@yousign.com>
2021-02-03 09:59:04 +0100

    ✏️ update blog title

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,7 @@ export default function Intro() {
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <Logo />
       <h1 className="typography-display font-bold tracking-tighter leading-tight md:pr-8">
-        Tech Blog.
+        Engineering & Product Blog.
       </h1>
     </section>
   );
