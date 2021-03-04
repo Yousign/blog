207a4c3bbb5f7b21eab25ec0e11c8fb46363dd07
jérôme boileux <jerome.boileux@yousign.com>
2021-03-04 17:21:57 +0100

    🔒 fix security clickjacking

--- next.config.js
+++ next.config.js
@@ -2,4 +2,17 @@ module.exports = {
   images: {
     domains: ['ys-storage-public-blogtech-content-bucket.s3.eu-west-3.amazonaws.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
 };
