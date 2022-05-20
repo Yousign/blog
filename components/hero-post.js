0e4708934e45e1643745f4ede7eae03e15ad42b0
jérôme boileux <jerome.boileux@yousign.com>
2022-05-20 14:15:41 +0200

    💄 make hero-post height fluid

--- components/hero-post.js
+++ components/hero-post.js
@@ -8,7 +8,7 @@ export default function HeroPost({ title, coverImage, date, authors, slug }) {
     <section className="hero-post md:container md:mx-auto mb-8">
       <div className="h-full grid grid-cols-1 md:grid-cols-12 bg-purple-light text-white md:rounded-md overflow-hidden">
         <div className="md:col-span-7">
-          <CoverImage title={title} src={coverImage} slug={slug} width={768} height={468} />
+          <CoverImage title={title} src={coverImage} slug={slug} layout="fill" />
         </div>
         <div className="md:col-span-5 p-4 lg:px-14 lg:py-10 flex flex-col">
           <div className="mb-4">
